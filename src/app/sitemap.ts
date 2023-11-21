19e0a31e45b2c503a36a3c9c2cf43deb81a1b4da
Luan-Patrik <luanpereira722@gmail.com>
2023-11-21 03:25:48 -0300

    fix: sso optimization

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,18 +2,29 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://luanpatrik.com'
-
   return [
     {
-      url: `${baseUrl}`,
+      url: `${baseUrl}/pt`,
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/pt/about`,
       lastModified: new Date(),
     },
     {
-      url: `${baseUrl}/projects`,
+      url: `${baseUrl}/pt/projects`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/en`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/en/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/en/projects`,
       lastModified: new Date(),
     },
   ]
