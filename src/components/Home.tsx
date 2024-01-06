5619eadb1a27fa084d052049341dabcff9d5ed88
Luan-Patrik <luanpereira722@gmail.com>
2024-01-06 03:31:39 -0300

    feat: adjusted responsiveness

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
               <h1 className='text-base font-normal md:text-xl'>
                 Oi, sou Luan Patrik 👋
               </h1>
-              <h2 className='xs:text-3xl text-2xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl'>
+              <h2 className='xs:text-3xl text-2xl font-extrabold sm:text-4xl md:text-5xl 2xl:text-7xl'>
                 DESENVOLVEDOR
                 <br />
                 FRONT-END
