5ef4a7468f0af2d73311e22dee5b1b9d2c4c23a7
Luan-Patrik <luanpereira722@gmail.com>
2024-01-15 21:52:24 -0300

    fix: attempt to fix error

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,4 +1,4 @@
-import ProjectsList from '@/components/projects/ProjectsList'
+import AllProjects from '@/components/projects/AllProjects'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -9,5 +9,5 @@ export const metadata: Metadata = {
   },
 }
 export default function CurriculumPage() {
-  return <ProjectsList />
+  return <AllProjects />
 }
--- src/components/Projects/AllProjects.tsx
+++ src/components/Projects/AllProjects.tsx
@@ -3,18 +3,14 @@ import { revalidateTag } from 'next/cache'
 import Project from './Project'
 
 const ProjectsList = async () => {
-  // const posts = await useGetProjects()
+  const posts = await useGetProjects()
 
-  // if (!posts) return
-
-  // revalidateTag('projects')
+  revalidateTag('projects')
 
   return (
     <section className='container flex min-h-[calc(100svh_-_92px)] items-center justify-center'>
       <div className='grid max-w-5xl grid-cols-1 place-content-center gap-4 md:grid-cols-2'>
-        {/* {posts.map((post) => (
-          <Project key={post.id} {...post} />
-        ))} */}
+        {posts && posts.map((post) => <Project key={post.id} {...post} />)}
       </div>
     </section>
   )
