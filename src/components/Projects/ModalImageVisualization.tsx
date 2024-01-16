a45d0205d3b70969b49f98b80b0ac1378b22708b
Luan-Patrik <luanpereira722@gmail.com>
2024-01-15 21:15:51 -0300

    fix: attempt to fix error

--- src/components/Projects/ModalImageVisualization.tsx
+++ src/components/Projects/ModalImageVisualization.tsx
@@ -17,7 +17,7 @@ const ModalImageVisualization = ({
   title,
 }: ModalImageVisualizationProps) => {
   return (
-    <div className='= container fixed'>
+    <div className='container fixed'>
       <Dialog modal open={open} onOpenChange={openDialogMenu}>
         <DialogContent className='max-w-7xl rounded-md p-0'>
           <Image
--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -5,12 +5,16 @@ import Project from './Project'
 const ProjectsList = async () => {
   const posts = await useGetProjects()
 
+  if (!posts) return
+
   revalidateTag('projects')
 
   return (
     <section className='container flex min-h-[calc(100svh_-_92px)] items-center justify-center'>
       <div className='grid max-w-5xl grid-cols-1 place-content-center gap-4 md:grid-cols-2'>
-        {posts && posts.map((post) => <Project key={post.id} {...post} />)}
+        {posts.map((post) => (
+          <Project key={post.id} {...post} />
+        ))}
       </div>
     </section>
   )
