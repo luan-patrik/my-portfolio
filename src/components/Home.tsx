c27faf33189983105e1989b10ff6d539bf6e1fc6
Luan-Patrik <luanpereira722@gmail.com>
2024-02-13 02:34:06 -0300

    chore: updated default information's

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ import { buttonVariants } from './ui/button'
 
 export const Home = () => {
   return (
-    <section className='flex-1 pt-4 lg:sticky lg:top-16 lg:h-full lg:w-full lg:min-w-96 lg:py-4'>
+    <section className='flex flex-1 flex-col items-center pt-4 sm:block sm:items-start lg:sticky lg:top-16 lg:h-full lg:w-full lg:min-w-96 lg:py-4'>
       <Image
         src={'/assets/photo.webp'}
         width={180}
@@ -17,12 +17,15 @@ export const Home = () => {
         priority
         className='select-none rounded-full border-2 object-cover shadow-lg'
       />
-      <div className='flex flex-col items-start gap-4'>
-        <h1 className='text-4xl font-bold'>Luan Patrik</h1>
-        <h2 className='text-xl font-light'>
+      <div className='flex flex-col items-center gap-4 text-center sm:items-start sm:text-start'>
+        <h1 className='text-2xl font-bold xs:text-4xl'>Luan Patrik</h1>
+        <h2 className='text-base font-light xs:text-lg sm:text-xl'>
           Desenvolvedor <span className='whitespace-nowrap'>Front-End</span>
         </h2>
-        <p className='inline-flex items-center gap-2 text-xl font-light'>
+        <p className='text-base font-light xs:text-lg sm:text-xl'>
+          Há 3 anos criando projetos desafiadores.
+        </p>
+        <p className='inline-flex items-center gap-2 text-base font-light xs:text-lg sm:text-xl'>
           Brasil <Icon.flag className='size-6' />
         </p>
         <div className='flex w-full flex-col items-center gap-4 sm:flex-row'>
